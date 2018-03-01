e63115199e5068f8850b00578f68c5d2784a33c8
tomming233 <tomming233@gmail.com>
2018-03-01 17:50:12 +0800

    cp17: 期物，并发编程

---
+++ cp17/flags_threadpool_ac.py
@@ -0,0 +1,74 @@
+"""Download flags of top 20 countries by population
+Sequential version
+Sample run::
+    $ python3 flags.py
+    BD BR CD CN DE EG ET FR ID IN IR JP MX NG PH PK RU TR US VN
+    20 flags downloaded in 10.16s
+"""
+# BEGIN FLAGS_PY
+import os
+import time
+import sys
+
+import requests  # <1>
+from concurrent import futures
+
+POP20_CC = ('CN IN US ID BR PK NG BD RU JP '
+            'MX PH VN ET EG DE IR TR CD FR').split()  # <2>
+
+BASE_URL = 'http://flupy.org/data/flags'  # <3>
+
+DEST_DIR = 'downloads/'  # <4>
+
+
+def save_flag(img, filename):  # <5>
+    path = os.path.join(DEST_DIR, filename)
+    with open(path, 'wb') as fp:
+        fp.write(img)
+
+
+def get_flag(cc):  # <6>
+    url = '{}/{cc}/{cc}.gif'.format(BASE_URL, cc=cc.lower())
+    resp = requests.get(url)
+    return resp.content
+
+
+def show(text):  # <7>
+    print(text, end=' ')
+    sys.stdout.flush()
+
+
+def download_one(cc):
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + '.gif')
+    return cc
+
+
+def download_many(cc_list):  # <8>
+    cc_list = cc_list[:5]
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+        to_do = []
+        for cc in sorted(cc_list):
+            future = executor.submit(download_one, cc)
+            to_do.append(future)
+            msg = 'Scheduled for {}: {}'
+            print(msg.format(cc, future))
+        results = []
+        for future in futures.as_completed(to_do):
+            res = future.result()
+            msg = '{} result: {!r}'
+            print(msg.format(future, res))
+            results.append(res)
+    return len(results)
+
+def main(download_many):  # <10>
+    t0 = time.time()
+    count = download_many(POP20_CC)
+    elapsed = time.time() - t0
+    msg = '\n{} flags downloaded in {:.2f}s'
+    print(msg.format(count, elapsed))
+
+
+if __name__ == '__main__':
+    main(download_many)  # <11>
