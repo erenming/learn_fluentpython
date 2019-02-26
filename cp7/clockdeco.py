320888090143d30b248b4e71138cdbaf49f4ce35
tomming233 <tomming233@gmail.com>
2019-02-26 23:52:56 +0800

    learning

--- cp7/clockdeco.py
+++ cp7/clockdeco.py
@@ -23,15 +23,21 @@ DEFAULT_FMT = '[{elapsed:0.8f}s] {name}({args}) -> {result}'
 #         return result
 #     return clocked
 
-def clock(fmt=DEFAULT_FMT):
+def clock(fmt='[{elapsed:0.8f}s] {name}({args}) -> {result}'):
     def decorate(func):
+        @functools.wraps(func)
         def clocked(*args):
-            t0 = time.perf_counter()
             result = func(*args)
-            elapsed = time.perf_counter() - t0
             name = func.__name__
             arg_str = ', '.join(repr(arg) for arg in args)
-            print(fmt % (elapsed, name, arg_str, result))
+            print(fmt)
             return result
         return clocked
     return decorate
+
+@clock()
+def f():
+    return 1
+
+if __name__ == "__main__":
+    f()
